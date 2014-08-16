176e4b46ec4882bad3ac062df950d97d04288e9f
fiddleplum <stephenhurley@gmail.com>
2014-08-15 21:51:35 -0700

    Made it stable

--- src/kit/main.cpp
+++ src/kit/main.cpp
@@ -3,12 +3,37 @@
 #include "log.h"
 #include <SDL.h>
 
-extern int entry(std::vector<std::string> const & args);
+//extern int entry(std::vector<std::string> const & args);
 
 // Called by SDL to run the entire application.
 int main(int argc, char *argv[])
 {
-	int r = 0;
+	//int r = 0;
+
+	//try
+	//{
+	//	// Grab the params.
+	//	std::vector<std::string> args;
+	//	for(int i = 1; i < argc; ++i) // don't include the 0th arg, because it is the program name
+	//	{
+	//		args.push_back(std::string(argv[i]));
+	//	}
+
+	//	// Call the user-supplied entry point.
+	//	r = entry(args);
+	//}
+	//catch(std::runtime_error const & e)
+	//{
+	//	SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error!", e.what(), nullptr);
+	//	return -1;
+	//}
+	//catch(std::exception)
+	//{
+	//	SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Fatal Error!", "The application encountered something so horrendous that it died instantly!", nullptr);
+	//	return -1;
+	//}
+
+	//return r;
 
 	try
 	{
@@ -19,8 +44,15 @@ int main(int argc, char *argv[])
 			args.push_back(std::string(argv[i]));
 		}
 
-		// Call the user-supplied entry point.
-		r = entry(args);
+		kit::app::initialize();
+
+		kit::start(args);
+
+		kit::app::loop();
+
+		kit::finish();
+
+		kit::app::finalize();
 	}
 	catch(std::runtime_error const & e)
 	{
@@ -33,35 +65,6 @@ int main(int argc, char *argv[])
 		return -1;
 	}
 
-	return r;
-
-	/*
-	try
-	{
-	kit::app::initialize();
-
-	kit::start(parameters);
-
-	kit::app::loop();
-
-	kit::finish();
-
-	kit::app::finalize();
-	}
-	catch(std::runtime_error const & e)
-	{
-	kit::log::write(std::string("Error: ") + e.what());
-	SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error!", e.what(), nullptr);
-	return -1;
-	}
-	catch(std::exception)
-	{
-	kit::log::write("Fatal error!");
-	SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Fatal Error!", "The application encountered something so horrendous that it died instantly!", nullptr);
-	return -1;
-	}
-
 	return 0;
-	*/
 }
 
